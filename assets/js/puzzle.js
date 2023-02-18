6c24369f55796d459a8ae122a550384cd47ac9f3
ravib7 <ravibchaudhri7@gmail.com>
2023-02-18 16:49:55 +0530

    first version of puzzle with 0 to 9 values 3x3 matrix

--- assets/js/puzzle.js
+++ assets/js/puzzle.js
@@ -14,15 +14,37 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
-   
+
 }
 
-function showMessage() {
-    alert("Congratulations if you finished!");
+const myButton = document.querySelector(".save");
+
+function checkSorted(array) {
+
+    if (array.length == 9) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > array[i + 1]) {
+                alert("The numbers are not arranged sequentially!!!!");
+                return false;
+            }
+            else {
+                continue;
+            }
+
+        }
+        myButton.disabled = false;
+        alert("Great job :)");
+    }
+    else {
+        alert("please arrange all numbers!!!")
+    }
 }
-function chkFillSeq(){
+
+
+
+function chkFillSeq() {
     // var board = document.getElementById('text');
-    // for (var i=0; i<board.length; i++) {
+    // for (var i = 0; i < board.length; i++) {
     //     console.log(board[i]);
     // }
     debugger;
@@ -38,13 +60,77 @@ function chkFillSeq(){
     // iterate over all child div elements and check whether they contain an image
     childDivs.forEach(childDiv => {
         const images = childDiv.querySelectorAll('img');
-        if (images.length > 0) {                        
-            //console.log(images.length);
+        if (images.length > 0) {
+            // console.log(images.length);
             divsWithImages.push(images[0].id.substr(4));
         }
     });
-
     // divsWithImages array now contains all child div elements that contain an image tag
     console.log(divsWithImages);
-    alert(divsWithImages);
+    // alert(divsWithImages);
+    checkSorted(divsWithImages);
 }
+
+
+
+
+
+
+
+const Button = document.querySelector(".save");
+
+function checkSorted(array) {
+
+    if (array.length == 9) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > array[i + 1]) {
+                alert("The numbers are not arranged sequentially!!!!");
+                return false;
+            }
+            else {
+                continue;
+            }
+
+        }
+        Button.disabled = false;
+        alert("Great job :)" );
+    }
+    else {
+        alert("please arrange all numbers!!!")       
+    }
+
+}
+
+
+
+function showMessage() {
+    // var board = document.getElementById('text');
+    // for (var i = 0; i < board.length; i++) {
+    //     console.log(board[i]);
+    // }
+    debugger;
+    // get the parent element
+    const parent = document.getElementById('text');
+
+    // get all child div elements
+    const childDivs = parent.querySelectorAll('div');
+
+    // create an empty array to store the div elements that contain an image
+    const divsWithImages = [];
+
+    // iterate over all child div elements and check whether they contain an image
+    childDivs.forEach(childDiv => {
+        const images = childDiv.querySelectorAll('img');
+        if (images.length > 0) {
+            // console.log(images.length);
+            divsWithImages.push(images[0].id.substr(4));
+        }
+    });
+    // divsWithImages array now contains all child div elements that contain an image tag
+    console.log("showMessage()");
+    // alert(divsWithImages);
+    checkSorted("showMessage()");
+}
+
+
+
