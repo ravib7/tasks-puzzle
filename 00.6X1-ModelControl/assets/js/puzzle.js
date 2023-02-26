3d3491b67828a6b7c3ae3894ff4f920a8c0b92ba
ravib7 <ravibchaudhri7@gmail.com>
2023-02-26 16:03:17 +0530

    ipv4-ipv6

---
+++ 00.6X1-ModelControl/assets/js/puzzle.js
@@ -0,0 +1,47 @@
+
+const myButton = document.querySelector(".save");
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
+function chkFillSeq() {
+  const parent = document.getElementById("text");
+  const childDivs = parent.querySelectorAll("div");
+  const divsWithImages = [];
+
+  childDivs.forEach((childDiv) => {
+    const images = childDiv.querySelectorAll("img");
+    if (images.length > 0) {
+      divsWithImages.push(images[0].id.substr(4));
+    }
+  });
+
+  checkSorted(divsWithImages);
+}
+
+function checkSorted(array) {
+  if (array.length === 5) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > array[i + 1]) {
+        alert("The numbers are not arranged sequentially!");
+        return false;
+      }
+    }
+    alert("Congratulation You Completed Puzzle Successfully!");
+  } else {
+    alert("Please arrange all numbers!");
+  }
+}
+
+myButton.addEventListener("click", chkFillSeq);
